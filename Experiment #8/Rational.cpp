5a9c6f6019fa7a0268b262793d1c964eff258708
Doğukan Kaan Bozkurt <62968201+dkbozkurt@users.noreply.github.com>
2020-12-07 22:50:31 +0300

    OOP Experiment#8

---
+++ Experiment #8/Rational.cpp
@@ -0,0 +1,102 @@
+//Dogukan Kaan Bozkurt
+
+#include "Rational.h"
+#include <iostream>
+#include <cmath>
+#include "Rational.h"
+using namespace std;
+
+Rational::Rational(double n_,double d_)
+{
+	num=n_;
+	den=d_;
+}
+
+Rational::~Rational()
+{
+}
+///
+ostream &operator<<(ostream &output,const Rational &out )
+{
+	output<<out.num <<"/"<< out.den;
+	return output;	
+}
+istream &operator>>(istream &inp,Rational &in)
+{
+	inp>>in.num;
+	cout <<"/";
+	inp>>in.den;
+	return inp;
+}
+///
+Rational Rational::operator+(const Rational& p){
+    Rational temp;
+    temp.num=(*this).num*p.den+(*this).den*p.num;
+    temp.den=(*this).den*p.den;
+    return temp;
+}
+Rational Rational::operator-(const Rational& s){
+    Rational temp1;
+    temp1.num=(*this).num*s.den-(*this).den*s.num;
+    temp1.den=(*this).den*s.den;
+    return temp1;
+
+}
+Rational Rational::operator*(const Rational& m){
+    Rational temp2;
+    temp2.num=(*this).num*m.num;
+    temp2.den=(*this).den*m.den;
+    return temp2;
+
+}
+Rational Rational::operator/(const Rational& d){
+    Rational temp3;
+    temp3.num=(*this).num*d.den;
+    temp3.den=(*this).den*d.num;
+    return temp3;
+
+}
+///
+bool Rational::operator ==(const Rational& e){	//e=n2,this=n1
+	
+	if(((*this).num/(*this).den)==(e.num/e.den))
+		return 1;
+	else
+		return 0;
+}
+
+bool Rational::operator !=(const Rational& ne){
+	
+	if(((*this).num/(*this).den)!=(ne.num/ne.den) )
+		return 1;
+	else
+		return 0;	
+}
+bool Rational::operator <(const Rational& l){
+	
+	if(((*this).num/(*this).den)<(l.num/l.den))
+		return 1;
+	else
+		return 0;	
+}
+bool Rational::operator >(const Rational& g){
+	
+	if(((*this).num/(*this).den)>(g.num/g.den))
+		return 1;
+	else
+		return 0;	
+}
+bool Rational::operator <=(const Rational& le){
+	
+	if(((*this).num/(*this).den)<=(le.num/le.den))
+		return 1;
+	else
+		return 0;	
+}
+bool Rational::operator >=(const Rational& ge){
+	
+	if(((*this).num/(*this).den)==(ge.num/ge.den))
+		return 1;
+	else
+		return 0;	
+}
---
+++ Experiment #8/main.cpp
@@ -0,0 +1,47 @@
+//Dogukan Kaan Bozkurt
+
+#include <iostream>
+#include <cmath>
+#include "Rational.h"
+using namespace std;
+
+int main(int argc, char** argv) {
+	Rational n1,n2,x;
+	
+	cout << "Enter rational number in the form num/dem:" << endl;
+	cin >> n1 >> n2;
+	
+	cout <<"n1: "<< n1 << endl <<"n2: "<< n2 << endl <<"default x: "<< x << endl;
+	
+	x=n1+n2;
+	cout << n1 << " + " << n2 << " = " << x << endl;
+	
+	x=n1-n2;
+	cout << n1 << " - " << n2 << " = " << x << endl;
+
+	x=n1*n2;
+	cout << n1 << " * " << n2 << " = " << x << endl;
+	
+	x=n1/n2;
+	cout << n1 << " / " << n2 << " = " << x << endl;
+///	
+	if(n1<n2)
+		cout << "n1 is less than n2" << endl;
+		
+	if(n1>n2)
+		cout << "n1 is greater than n2" << endl;		
+
+	if(n1==n2)
+		cout << "n1 is equal to n2" << endl;		
+
+	if(n1!=n2)
+		cout << "n1 is not equal to n2" << endl;		
+
+	if(n1<=n2)
+		cout << "n1 is less than or equal to n2" << endl;		
+
+	if(n1>=n2)
+		cout << "n1 is greater than or equal to n2" << endl;		
+	
+	return 0;
+}
