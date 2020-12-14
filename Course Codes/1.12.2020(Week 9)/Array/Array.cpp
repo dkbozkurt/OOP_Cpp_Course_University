0757e19b66f346f4c1771b929f01888543f5f7a9
Doğukan Kaan Bozkurt <62968201+dkbozkurt@users.noreply.github.com>
2020-12-14 22:40:00 +0300

    Course Codes

---
+++ Course Codes/1.12.2020(Week 9)/Array/Array.cpp
@@ -0,0 +1,119 @@
+//Dogukan Kaan Bozkurt
+
+#include "Array.h"
+#include <iostream>
+#include <stdexcept>
+#include <iomanip>
+using namespace std;
+
+Array::Array(int arraySize) //Default constr.
+{
+	if (arraySize > 0)
+		size=arraySize;
+	else
+		throw invalid_argument("Array size must be greater than 0");
+	
+	ptr=new int[size]; //create space for array *************************
+	
+	for(int i=0;i<size;++i)
+		ptr[i]=0;	//set array element
+}
+Array::Array(const Array &arrayToCopy)	// Copy constructor for class Array
+	:size(arrayToCopy.size)	//arrayToCopy const oldugu icin size i burada tanimladik. ( obj const oldugu icin)******************************
+{
+	cout << "size "<<size << endl;
+	ptr=new int[size];
+	
+	for(int i=0;i<size;++i)
+		ptr[i]=arrayToCopy.ptr[i];	//set array element
+}
+
+Array::~Array()	 //Destructor for class Array
+{
+	delete [] ptr ; //release array space 
+	//The destructor uses delete[] to release the memory allocated dynamically by new in the constructor.
+}
+
+int Array::getSize() const
+{
+	return size;
+}
+const Array &Array::operator=(const Array &r)
+{
+	if(&r != this){	//avoid self-assignment
+		
+		if(size != r.size){
+			delete [] ptr;	//release space	//thisi sildik.
+			size=r.size;	//resize this object	this in sizeina r nin sizesini esitledik
+			ptr=new int[size];	//create space for array copy	//ptr burada this olur.
+		}
+		
+		for (int i=0;i< size;++i)
+			ptr[i]=r.ptr[i];	//copy array into object
+	}	
+	
+	return *this;
+} 
+
+bool Array::operator==(const Array &r) const
+{
+	if(size!=r.size)	//sizeleri kontrol ediyoruz.
+		return false;
+		
+	for (int i=0;i<size;++i)
+		if(ptr[i] != r.ptr[i])
+			return false;	//Array contents are not equal
+			
+	return true;	//Arrays are equal
+}
+
+bool Array::operator!=(const Array &r) const
+{
+	return !(*this == r); //invokes operator ==
+}
+
+int &Array::operator[](int a)
+{
+	if(a<0 || a>=size)
+		throw out_of_range("Subscriptout of range");
+	
+	return ptr[a];	//reference return
+}
+int Array::operator[](int a) const
+{
+	if(a<0 || a>=size)
+		throw out_of_range("Subscriptout of range");
+	
+	return ptr[a];	//return copy of this element
+}
+istream&operator>>(istream &input,Array &a)	//input values for entire Array
+{
+	for(int i=0;i<a.size;++i)
+		input >> a.ptr[i];
+		
+	return input;	//enables cin >> x >> y;
+}
+ostream&operator<<(ostream &output,const Array &a)
+{
+	int i;
+	
+	for(i=0;i<a.size;++i){
+		output << setw(12) << a.ptr[i];
+		
+		if((i+1)%4 == 0)
+			output << endl;
+	}
+	
+	if (i%4 !=0)
+		output << endl;
+/*	
+	return output;
+		for(int i=0;i<x.size;i++){
+		output<<x.ptr[i];
+		if(i<x.size-1)
+			cout << ",";
+	}	
+	return output;
+	*/// gibi de yazilabilirdi sade halde.
+}
+
---
+++ Course Codes/1.12.2020(Week 9)/Quiz calisma/quiz2calisma.h
@@ -0,0 +1,35 @@
+//Dogukan Kaan Bozkurt
+
+#ifndef QUIZ2CALISMA_H
+#define QUIZ2CALISMA_H
+
+#include <iostream>
+using namespace std;
+
+class quiz2calisma
+{
+	
+	friend ostream&operator<<(ostream &,const quiz2calisma&);	
+	friend istream&operator>>(istream&,quiz2calisma&);
+	
+public:
+	
+	quiz2calisma(int size=4);	//size buradaki index sayisi yani max Array[3]
+	quiz2calisma(const quiz2calisma& );
+	~quiz2calisma();
+	
+	int getSize() const;
+	
+	quiz2calisma& operator*(void);	//.append() gibi calisir size i 1 büyütüp icine 0-9 arasi bir rakam atar, ama bas tarafina.
+	quiz2calisma& operator++(int); 	//.append() gibi calisir size i 1 büyütüp icine 0-9 arasi bir rakam atar.
+	const quiz2calisma& operator=(const quiz2calisma&);
+	bool operator==(const quiz2calisma& );
+	
+	
+private:
+	int size;
+	int *ptr;
+	
+};
+
+#endif
